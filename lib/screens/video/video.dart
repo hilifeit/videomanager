8a3b056acedcba4d417b0949d3821701a0c1e122
evampiro <sanimshrestha.60@gmail.com>
2022-06-09 17:36:41 +0545

    layout for videoplayer

--- lib/screens/video/video.dart
+++ lib/screens/video/video.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:video_player/video_player.dart';
 import 'package:flutter/material.dart';
 
@@ -9,7 +10,6 @@ class Video extends StatefulWidget {
 class _VideoState extends State<Video> {
   late VideoPlayerController _controller1;
   late VideoPlayerController _controller2;
-
 
   @override
   void initState() {
@@ -33,38 +33,82 @@ class _VideoState extends State<Video> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Video Demo',
-      home: Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              flex: 2,
-              child: Row(
-                children: [
-                  Expanded(child: VideoPlayer(_controller1)),
-                  Expanded(child: VideoPlayer(_controller2)),
-                ],
-              ),
-            ),
-            Expanded(
-              flex:1, child: Placeholder())
-          ],
-        ),
-                     floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _controller1.value.isPlaying
-                  ? _controller1.pause()
-                  : _controller1.play();
-              _controller2.value.isPlaying
-                  ? _controller2.pause()
-                  : _controller2.play();
-            });
-          },
-          child: Icon(
-            _controller1.value.isPlaying ? Icons.pause : Icons.play_arrow,
+    return Scaffold(
+      body: Column(
+        children: [
+          Placeholder(
+            fallbackHeight: 101.sm,
+            color: Colors.blue,
           ),
+          Flexible(
+            flex: 6,
+            child: Row(
+              children: [
+                Expanded(
+                    child: Column(
+                  children: [
+                    Expanded(flex: 4, child: VideoPlayer(_controller1)),
+                    // Expanded(
+                    //   flex: 1,
+                    //   child: Placeholder(),
+                    // )
+                  ],
+                )),
+                Expanded(child: VideoPlayer(_controller2)),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 3,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Placeholder(
+                          color: Colors.green,
+                          fallbackHeight: 50.sm,
+                        ),
+                      ),
+                      Expanded(
+                        child: Placeholder(
+                          color: Colors.green,
+                          fallbackHeight: 50.sm,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Placeholder(
+                    color: Colors.red,
+                    fallbackHeight: 65.sm,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // Placeholder(
+          //   fallbackHeight: 50.sm,
+          // )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _controller1.value.isPlaying
+                ? _controller1.pause()
+                : _controller1.play();
+            _controller2.value.isPlaying
+                ? _controller2.pause()
+                : _controller2.play();
+          });
+        },
+        child: Icon(
+          _controller1.value.isPlaying ? Icons.pause : Icons.play_arrow,
         ),
       ),
     );
@@ -77,57 +121,3 @@ class _VideoState extends State<Video> {
     _controller2.dispose();
   }
 }
-
-
-
-
-
-
-// import 'package:flutter/material.dart';
-// import 'package:flutter/src/foundation/key.dart';
-// import 'package:flutter/src/widgets/framework.dart';
-// import 'package:video_player/video_player.dart';
-
-// class Video extends StatefulWidget {
-//   Video({Key? key}) : super(key: key);
-
-//   @override
-//   State<Video> createState() => _VideoState();
-// }
-
-// class _VideoState extends State<Video> {
-//   late VideoPlayerController _controller;
-
-//   @override
-//   void initState() {
-//     // TODO: implement initState
-//     super.initState();
-//     _controller = VideoPlayerController.network(
-//       'http://192.168.1.10:8000/video/long-sample.mp4',
-//     )..initialize().then((value) 
-//     {
-//         setState(() {});
-//         _controller.position.asStream().listen((event) {
-//           print(event);
-//         });
-//         print(_controller.value.position);
-//       });
-//       _controller.addListener(() {
-//       print(_controller.value.position);
-//     });
-//   }
-
-
-
-//   @override
-//   Widget build(BuildContext context) {
-    
-//     return Scaffold(
-//         body: _controller.value.isInitialized
-//             ? AspectRatio(
-//                 aspectRatio: _controller.value.aspectRatio,
-//                 child: VideoPlayer(_controller),
-//               )
-//             : Container());
-//   }
-// }
