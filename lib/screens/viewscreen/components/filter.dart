9edc60f8767ece5ef6e6ab389bf3336ef77de7d2
Rajiv <rajiv24joshi@gmail.com>
2022-06-13 16:07:27 +0545

    search bar tweaks

--- lib/screens/viewscreen/components/filter.dart
+++ lib/screens/viewscreen/components/filter.dart
@@ -46,6 +46,37 @@ class Filter extends StatelessWidget {
                             ref.watch(searchChangeNotifierProvider);
 
                         return SearchField<Result>(
+                            emptyWidget: fluent.Padding(
+                              padding:  EdgeInsets.all(15.r),
+                              child: fluent.Column(
+                                children: const [
+                                  Icon(Icons.error,color: primaryColor,),
+                                  Text('Your search did not match any documents.',
+                                      maxLines: 2),
+                                ],
+                              ),
+                            ),
+                            searchStyle: kTextStyleInterMedium,
+                            searchInputDecoration: InputDecoration(
+                              prefixIcon: Icon(Videomanager.search,
+                                  color: Colors.black, size: 15.sp),
+                              fillColor: secondaryColor,
+                              filled: true,
+                              contentPadding: EdgeInsets.only(
+                                  left: 10.5.w, top: 9.h, bottom: 11.h),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5.r),
+                                borderSide: const BorderSide(
+                                    color: Color(0xffD1D1D1), width: 1),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(5.r),
+                                borderSide: const BorderSide(
+                                    color: Color(0xffD1D1D1), width: 1),
+                              ),
+                              hintText: 'Search',
+                              hintStyle: kTextStyleInterMedium,
+                            ),
                             focusNode: focus,
                             itemHeight: 80.h,
                             // hasOverlay: false,
@@ -59,22 +90,37 @@ class Filter extends StatelessWidget {
                                   item.item!.startCoordinate.lat,
                                   item.item!.startCoordinate.lng);
                             },
-                            
                             suggestions: searchService.results
                                 .map((e) => SearchFieldListItem<Result>(
-                                    e.filename,
-                                    item: e,
-                  
-                                    child:ListTile(
+                                      e.filename,
+                                      item: e,
+                                      child: ListTile(
                                         dense: true,
                                         horizontalTitleGap: 0,
-                                        subtitle: Text('${e.area.area}, ${e.area.city}',style: kTextStyleIbmRegularBlack,),
-
-                                        contentPadding: fluent.EdgeInsets.only(right: 10.w,bottom: 0,left: 0,top: 0),
-                                        leading:  Icon(Videomanager.video_file,color: primaryColor,size: 22.r,),
-                                        title:Text(e.filename,style: kTextStyleIbmRegularBlack.copyWith(fontSize: 16.sp),),
-                                        trailing: Text( 'State ${e.area.state}',style: kTextStyleIbmRegularBlack,),
+                                        subtitle: Text(
+                                          '${e.area.area}, ${e.area.city}',
+                                          style: kTextStyleIbmRegularBlack,
                                         ),
+                                        contentPadding: fluent.EdgeInsets.only(
+                                            right: 10.w,
+                                            bottom: 0,
+                                            left: 0,
+                                            top: 0),
+                                        leading: Icon(
+                                          Videomanager.video_file,
+                                          color: primaryColor,
+                                          size: 22.r,
+                                        ),
+                                        title: Text(
+                                          e.filename,
+                                          style: kTextStyleIbmRegularBlack
+                                              .copyWith(fontSize: 16.sp),
+                                        ),
+                                        trailing: Text(
+                                          'State ${e.area.state}',
+                                          style: kTextStyleIbmRegularBlack,
+                                        ),
+                                      ),
                                     ))
                                 .toList());
 
@@ -118,80 +164,82 @@ class Filter extends StatelessWidget {
                   ),
                 ),
                 Expanded(
-                    child: ListView.separated(
-                  itemBuilder: (_, index) {
-                    return ExpansionTile(
+                  child: ListView.separated(
+                    itemBuilder: (_, index) {
+                      return ExpansionTile(
 
-                        // tilePadding: EdgeInsets.only(left: 0,right: 10),
-                        childrenPadding: EdgeInsets.only(left: 57.w),
-                        leading: Consumer(builder: (context, ref, c) {
-                          final checked =
-                              ref.watch(checkBoxStateStateProvider.state).state;
-                          return Checkbox(
-                              // visualDensity: VisualDensity.adaptivePlatformDensity,
-                              materialTapTargetSize:
-                                  MaterialTapTargetSize.padded,
-                              side: const BorderSide(
-                                width: 1,
-                                color: secondaryColorText,
-                              ),
-                              activeColor: primaryColor,
-                              value: checked,
-                              onChanged: (value) {
-                                ref
-                                    .read(checkBoxStateStateProvider.state)
-                                    .state = value!;
-                              });
-                        }),
-                        title: Text(
-                          'State',
-                          style:
-                              kTextStyleInterMedium.copyWith(fontSize: 16.sp),
-                        ),
-                        children: [
-                          ListTile(
-                            horizontalTitleGap: 0.r,
-                            // contentPadding: EdgeInsets.only(left: 0),
-                            leading: Consumer(builder: (context, ref, c) {
-                              final checked = ref
-                                  .watch(checkBoxStateStateProvider.state)
-                                  .state;
-                              return SizedBox(
-                                width: 15,
-                                height: 15,
-                                child: Checkbox(
-                                    materialTapTargetSize:
-                                        MaterialTapTargetSize.padded,
-                                    side: const BorderSide(
-                                      width: 1,
-                                      color: secondaryColorText,
-                                    ),
-                                    activeColor: primaryColor,
-                                    value: checked,
-                                    onChanged: (value) {
-                                      ref
-                                          .read(
-                                              checkBoxStateStateProvider.state)
-                                          .state = value!;
-                                    }),
-                              );
-                            }),
-                            title: Text(
-                              'State 1',
-                              style: kTextStyleInterMedium.copyWith(
-                                  fontSize: 16.sp),
-                            ),
+                          // tilePadding: EdgeInsets.only(left: 0,right: 10),
+                          childrenPadding: EdgeInsets.only(left: 57.w),
+                          leading: Consumer(builder: (context, ref, c) {
+                            final checked = ref
+                                .watch(checkBoxStateStateProvider.state)
+                                .state;
+                            return Checkbox(
+                                // visualDensity: VisualDensity.adaptivePlatformDensity,
+                                materialTapTargetSize:
+                                    MaterialTapTargetSize.padded,
+                                side: const BorderSide(
+                                  width: 1,
+                                  color: secondaryColorText,
+                                ),
+                                activeColor: primaryColor,
+                                value: checked,
+                                onChanged: (value) {
+                                  ref
+                                      .read(checkBoxStateStateProvider.state)
+                                      .state = value!;
+                                });
+                          }),
+                          title: Text(
+                            'State',
+                            style:
+                                kTextStyleInterMedium.copyWith(fontSize: 16.sp),
                           ),
-                        ]);
-                  },
-                  separatorBuilder: (_, index) {
-                    return const Divider(
-                      height: 1,
-                      thickness: 0.5,
-                    );
-                  },
-                  itemCount: 3,
-                ),),
+                          children: [
+                            ListTile(
+                              horizontalTitleGap: 0.r,
+                              // contentPadding: EdgeInsets.only(left: 0),
+                              leading: Consumer(builder: (context, ref, c) {
+                                final checked = ref
+                                    .watch(checkBoxStateStateProvider.state)
+                                    .state;
+                                return SizedBox(
+                                  width: 15,
+                                  height: 15,
+                                  child: Checkbox(
+                                      materialTapTargetSize:
+                                          MaterialTapTargetSize.padded,
+                                      side: const BorderSide(
+                                        width: 1,
+                                        color: secondaryColorText,
+                                      ),
+                                      activeColor: primaryColor,
+                                      value: checked,
+                                      onChanged: (value) {
+                                        ref
+                                            .read(checkBoxStateStateProvider
+                                                .state)
+                                            .state = value!;
+                                      }),
+                                );
+                              }),
+                              title: Text(
+                                'State 1',
+                                style: kTextStyleInterMedium.copyWith(
+                                    fontSize: 16.sp),
+                              ),
+                            ),
+                          ]);
+                    },
+                    separatorBuilder: (_, index) {
+                      return const Divider(
+                        height: 1,
+                        thickness: 0.5,
+                      );
+                    },
+                    itemCount: 3,
+                  ),
+                ),
               ],
             ),
           ),
