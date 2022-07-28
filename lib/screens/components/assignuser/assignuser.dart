d874e36383c6a7668bfe610302a7326b27e8611d
rajiv24joshi <rajiv24joshi@gmail.com>
2022-07-28 17:45:49 +0545

    area assign widget fnctional, assign area popup bug fix on closing

--- lib/screens/components/assignuser/assignuser.dart
+++ lib/screens/components/assignuser/assignuser.dart
@@ -47,159 +47,166 @@ class AssignManager extends ConsumerWidget {
       ),
       child: Form(
         key: _formKey,
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Container(
-            padding: EdgeInsets.only(left: 80.sw(), top: 9.sh()),
-            height: 42.sh(),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(8.sr()),
-                  topRight: Radius.circular(8.sr())),
-            ),
-            child: Text(
-              'Assign Managers',
-              style: kTextStyleIbmRegular.copyWith(
-                fontSize: 16.ssp(),
-                color: Colors.white,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 80.sw(), top: 9.sh()),
+              height: 42.sh(),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(8.sr()),
+                    topRight: Radius.circular(8.sr())),
+              ),
+              child: Text(
+                'Assign Managers',
+                style: kTextStyleIbmRegular.copyWith(
+                  fontSize: 16.ssp(),
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 47.sw()),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 18.sh(),
-                  ),
-                  Center(
-                    child: Text(
-                      'Please Choose the Manager from the drop down below',
-                      style: kTextStyleIbmRegularBlack.copyWith(
-                        fontSize: 16.ssp(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 47.sw()),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 18.sh(),
+                    ),
+                    Center(
+                      child: Text(
+                        'Please Choose the Manager from the drop down below',
+                        style: kTextStyleIbmRegularBlack.copyWith(
+                          fontSize: 16.ssp(),
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 30.sh(),
-                  ),
-                  // Text(
-                  //   fileDetail == null ? 'Areaname' : fileDetail!.filename,
-                  //   style: kTextStyleIbmRegular.copyWith(
-                  //     fontSize: 16..ssp(),
-                  //     color: danger,
-                  //   ),
-                  // ),
-                  SizedBox(
-                    height: 29.sh(),
-                  ),
-                  InputTextField(
-                    fillColor: Colors.white,
-                    title: 'Area Name',
-                    limit: true,
-                    limitNumber: 20,
-                    suffixText: '${files.length.toString()} Videos',
-                    suffixStyle: kTextStyleIbmMedium.copyWith(
-                      fontSize: 13.ssp(),
-                      color: primaryColor,
+                    SizedBox(
+                      height: 30.sh(),
                     ),
-                    validator: (val) => validateArea(val!),
-                    isVisible: true,
-                    onChanged: (val) {
-                      area.name = val;
-                    },
-                  ),
-                  SizedBox(
-                    height: 18.sh(),
-                  ),
-                  Text(
-                    'Manager',
-                    style: kTextStyleIbmSemiBold,
-                  ),
-                  SizedBox(
-                    height: 6.sh(),
-                  ),
-                  managerMenu.isNotEmpty
-                      ? Container(
-                          decoration: BoxDecoration(
-                              border:
-                                  Border.all(width: 1, color: secondaryColor)),
-                          child: CustomMenuDropDown(
-                              value: managerMenu.first,
-                              onChanged: (val) {
-                                area.assignedTo.id = val.value;
-                              },
-                              values: managerMenu,
-                              helperText: ''),
-                        )
-                      : const Text("Add Managers first"),
-                  SizedBox(
-                    height: 32.sh(),
-                  ),
-                  Wrap(
-                      children: List.generate(
-                    areaItems.length,
-                    (index) => Padding(
-                        padding: EdgeInsets.only(right: 16.sw()),
-                        child: AreaCard(
-                          item: areaItems[index],
-                        )),
-                  )),
-                  SizedBox(
-                    height: 58.sh(),
-                  ),
-                  OutlinedElevatedButtonCombo(
-                    outlinedButtonText: 'Cancel',
-                    elevatedButtonText: 'Confirm',
-                    center: true,
-                    onPressedElevated: () {
-                      if (_formKey.currentState!.validate()) {
-                        area.assignedBy.id = userService.loggedInUser.value!.id;
-                        if (area.assignedTo.id == '' &&
-                            managerMenu.isNotEmpty) {
-                          area.assignedTo.id = managerMenu.first.value;
-                        }
-
-                        var dataMap = area.toJson();
-                        dataMap
-                            .addAll({"files": files.map((e) => e.id).toList()});
-
-                        showDialog(
-                            context: context,
-                            builder: (context) {
-                              return CustomDialog(
-                                elevatedButtonText: 'Yes',
-                                onPressedElevated: () async {
-                                  try {
-                                    await ref
-                                        .read(fileDetailMiniServiceProvider)
-                                        .createAreaAndAssign(dataMap);
-                                    snack.success("Area Assigned Succesfully");
-                                  } catch (e, s) {
-                                    print("$e $s");
-                                    snack.error(e);
-                                  }
+                    // Text(
+                    //   fileDetail == null ? 'Areaname' : fileDetail!.filename,
+                    //   style: kTextStyleIbmRegular.copyWith(
+                    //     fontSize: 16..ssp(),
+                    //     color: danger,
+                    //   ),
+                    // ),
+                    SizedBox(
+                      height: 29.sh(),
+                    ),
+                    InputTextField(
+                      fillColor: Colors.white,
+                      title: 'Area Name',
+                      limit: true,
+                      limitNumber: 20,
+                      suffixText: '${files.length.toString()} Videos',
+                      suffixStyle: kTextStyleIbmMedium.copyWith(
+                        fontSize: 13.ssp(),
+                        color: primaryColor,
+                      ),
+                      validator: (val) => validateArea(val!),
+                      isVisible: true,
+                      onChanged: (val) {
+                        area.name = val;
+                      },
+                    ),
+                    SizedBox(
+                      height: 18.sh(),
+                    ),
+                    Text(
+                      'Manager',
+                      style: kTextStyleIbmSemiBold,
+                    ),
+                    SizedBox(
+                      height: 6.sh(),
+                    ),
+                    managerMenu.isNotEmpty
+                        ? Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 1, color: secondaryColor)),
+                            child: CustomMenuDropDown(
+                                value: managerMenu.first,
+                                onChanged: (val) {
+                                  area.assignedTo.id = val.value;
                                 },
-                              );
-                            });
-                      }
-                    },
-                    onPressedOutlined: () {
-                      // Navigator.of(context).overlay!.mounted;
-                      Navigator.pop(context);
-                    },
-                    width: 96.sw(),
-                    height: 32.sh(),
-                    spacing: 19.sw(),
-                  )
-                ],
+                                values: managerMenu,
+                                helperText: ''),
+                          )
+                        : const Text("Add Managers first"),
+                    SizedBox(
+                      height: 32.sh(),
+                    ),
+                    Wrap(
+                        children: List.generate(
+                      areaItems.length,
+                      (index) => Padding(
+                          padding: EdgeInsets.only(right: 16.sw()),
+                          child: AreaCard(
+                            item: areaItems[index],
+                          )),
+                    )),
+                    SizedBox(
+                      height: 58.sh(),
+                    ),
+                    OutlinedElevatedButtonCombo(
+                      outlinedButtonText: 'Cancel',
+                      elevatedButtonText: 'Confirm',
+                      center: true,
+                      onPressedElevated: () {
+                        if (_formKey.currentState!.validate()) {
+                          area.assignedBy.id =
+                              userService.loggedInUser.value!.id;
+                          if (area.assignedTo.id == '' &&
+                              managerMenu.isNotEmpty) {
+                            area.assignedTo.id = managerMenu.first.value;
+                          }
+
+                          var dataMap = area.toJson();
+                          dataMap.addAll(
+                              {"files": files.map((e) => e.id).toList()});
+
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return CustomDialog(
+                                  textSecond: "assign this area?",
+                                  elevatedButtonText: 'Yes',
+                                  onPressedElevated: () async {
+                                    try {
+                                      await ref
+                                          .read(fileDetailMiniServiceProvider)
+                                          .createAreaAndAssign(dataMap);
+                                      snack
+                                          .success("Area Assigned Succesfully");
+                                      Navigator.pop(context);
+                                    } catch (e, s) {
+                                      print("$e $s");
+                                      snack.error(e);
+                                    }
+                                  },
+                                );
+                              });
+                        }
+                      },
+                      onPressedOutlined: () {
+                        // Navigator.of(context).overlay!.mounted;
+                        Navigator.pop(context);
+                      },
+                      width: 96.sw(),
+                      height: 32.sh(),
+                      spacing: 19.sw(),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        ]),
+          ],
+        ),
       ),
     );
   }
