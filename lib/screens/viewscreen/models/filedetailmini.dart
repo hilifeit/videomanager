8429dcec5b05b017a5d9fe503fbeaa2a2170d712
evampiro <sanimshrestha.60@gmail.com>
2022-07-28 11:24:44 +0545

    area addition complete

--- lib/screens/viewscreen/models/filedetailmini.dart
+++ lib/screens/viewscreen/models/filedetailmini.dart
@@ -100,7 +100,7 @@ class Location {
         "coordinates": List<dynamic>.from(
             coordinates.map((x) => List<dynamic>.from(x.map((x) => x)))),
       };
-      static Location empty()=>Location(type: '', coordinates: []);
+  static Location empty() => Location(type: 'LineString', coordinates: []);
 }
 
 class Area {
