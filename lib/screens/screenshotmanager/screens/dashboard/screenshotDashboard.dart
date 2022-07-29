83bbdf4d5319c5f1887a2b3bfcc9feae0397b928
rajiv24joshi <rajiv24joshi@gmail.com>
2022-07-29 17:53:49 +0545

    screenshot screen

--- lib/screens/screenshotmanager/screens/dashboard/screenshotDashboard.dart
+++ lib/screens/screenshotmanager/screens/dashboard/screenshotDashboard.dart
@@ -68,7 +68,7 @@ class ScreenshotDashboard extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     if (ResponsiveLayout.isDesktop && thisUser.role < Roles.superAdmin.index) {
-      CustomOverlayEntry().showVideoTimeStamp();
+      // CustomOverlayEntry().showVideoTimeStamp();
     }
 
     return Align(
@@ -127,20 +127,32 @@ class ScreenshotDashboard extends HookConsumerWidget {
                         SizedBox(
                           width: 43.sw(),
                         ),
-                        Container(
-                          width: 50.sr(),
-                          height: 50.sr(),
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.white,
-                          ),
-                          alignment: Alignment.center,
-                          padding:
-                              EdgeInsets.only(right: 3.sw(), bottom: 3.sh()),
-                          child: Icon(
-                            Videomanager.camera,
-                            color: Theme.of(context).primaryColor,
-                            size: 24.ssp(),
+                        InkWell(
+                          onTap: () {
+                            if (CustomOverlayEntry().videoTimeStampOpen) {
+                              CustomOverlayEntry().closeVideoTimeStamp();
+                            }
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return ScreenShotScreen();
+                                });
+                          },
+                          child: Container(
+                            width: 50.sr(),
+                            height: 50.sr(),
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.white,
+                            ),
+                            alignment: Alignment.center,
+                            padding:
+                                EdgeInsets.only(right: 3.sw(), bottom: 3.sh()),
+                            child: Icon(
+                              Videomanager.camera,
+                              color: Theme.of(context).primaryColor,
+                              size: 24.ssp(),
+                            ),
                           ),
                         ),
                         SizedBox(
@@ -179,4 +191,82 @@ class ScreenshotDashboard extends HookConsumerWidget {
       ),
     );
   }
+}
+
+class ScreenShotScreen extends StatelessWidget {
+  const ScreenShotScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                color: whiteColor,
+                image: DecorationImage(
+                  opacity: 0.5,
+                  image: AssetImage("assets/images/wallpaper.jpg"),
+                  fit: BoxFit.cover,
+                )),
+            child: ClipPath(
+              clipper: ScreenShotClipper(),
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/wallpaper.jpg"),
+                        fit: BoxFit.cover)),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 46.sh(),
+            right: 54.sw(),
+            child: Row(
+              children: [
+                CustomOutlinedButton(
+                    borderColor: whiteColor,
+                    onPressedOutlined: () {},
+                    outlinedButtonText: 'Clear All'),
+                SizedBox(
+                  width: 25.sw(),
+                ),
+                CustomOutlinedButton(
+                    borderColor: whiteColor,
+                    onPressedOutlined: () {
+                      Navigator.pop(context);
+                    },
+                    outlinedButtonText: 'Cancel'),
+                SizedBox(
+                  width: 25.sw(),
+                ),
+                CustomElevatedButton(
+                    enabled: false,
+                    onPressedElevated: () {},
+                    elevatedButtonText: 'Confirm(5)')
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ScreenShotClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path()
+      ..addRect(Rect.fromCenter(
+          center: Offset(size.width / 2 - 50.sw(), size.height / 2 - 50.sh()),
+          width: size.width * 0.788,
+          height: size.height * 0.68));
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
--- lib/screens/users/component/userService.dart
+++ lib/screens/users/component/userService.dart
@@ -41,7 +41,6 @@ class UserService extends ChangeNotifier {
     if (userJson != null) {
       loggedInUser.value = UserModelMini.fromJson(userJson);
     }
-    
   }
 
   store() async {
@@ -52,11 +51,11 @@ class UserService extends ChangeNotifier {
   // Future<UserModel>
   fetchOne(String id) async {
     try {
-      var response = await tunnelRequest(
-          () => client.get(Uri.parse("${CustomIP.apiBaseUrl}user/$id"), headers: {
-                "Content-Type": "application/json",
-                "x-access-token": loggedInUser.value!.accessToken!
-              }));
+      var response = await tunnelRequest(() =>
+          client.get(Uri.parse("${CustomIP.apiBaseUrl}user/$id"), headers: {
+            "Content-Type": "application/json",
+            "x-access-token": loggedInUser.value!.accessToken!
+          }));
       if (response.statusCode == 200) {
         selectedUser.value = userModelFromJson(response.body);
         notifyListeners();
@@ -71,11 +70,11 @@ class UserService extends ChangeNotifier {
   fetchAll() async {
     // try {
     if (loggedInUser.value != null) {
-      var response = await tunnelRequest(
-          () => client.get(Uri.parse("${CustomIP.apiBaseUrl}$userEndPoint"), headers: {
-                "Content-Type": "application/json",
-                "x-access-token": loggedInUser.value!.accessToken!
-              }));
+      var response = await tunnelRequest(() => client
+              .get(Uri.parse("${CustomIP.apiBaseUrl}$userEndPoint"), headers: {
+            "Content-Type": "application/json",
+            "x-access-token": loggedInUser.value!.accessToken!
+          }));
 
       if (response.statusCode == 200) {
         var temp = userModelMiniListFromJson(response.body);
@@ -120,15 +119,15 @@ class UserService extends ChangeNotifier {
       required String password,
       required bool remember}) async {
     try {
-   
-      var response = await client.post(Uri.parse("${CustomIP.apiBaseUrl}auth/login"),
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: jsonEncode({
-            "username": username,
-            "password": password,
-          }));
+      var response =
+          await client.post(Uri.parse("${CustomIP.apiBaseUrl}auth/login"),
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: jsonEncode({
+                "username": username,
+                "password": password,
+              }));
       if (response.statusCode == 200) {
         var temp = userModelMiniFromJson(response.body);
         // if (temp.role == Roles.user.index) {
@@ -144,7 +143,7 @@ class UserService extends ChangeNotifier {
         return true;
       } else {
         var error = jsonDecode(response.body);
-     
+
         throw error['message'];
       }
     } catch (e, s) {
@@ -216,20 +215,21 @@ class UserService extends ChangeNotifier {
 
   Future<bool> add({required AddNewUser addUser}) async {
     try {
-      var response = await client.post(Uri.parse("$CustomIP.apiBaseUrl$userEndPoint"),
-          headers: {
-            "Content-Type": "application/json",
-            "x-access-token": loggedInUser.value!.accessToken!
-          },
-          body: jsonEncode({
-            "username": addUser.username,
-            "password": addUser.password,
-            "mobile": addUser.mobile,
-            "name": addUser.name,
-            "role": addUser.role,
-            "email": addUser.email,
-            "superVisor": addUser.superVisor.id
-          }));
+      var response =
+          await client.post(Uri.parse("${CustomIP.apiBaseUrl}$userEndPoint"),
+              headers: {
+                "Content-Type": "application/json",
+                "x-access-token": loggedInUser.value!.accessToken!
+              },
+              body: jsonEncode({
+                "username": addUser.username,
+                "password": addUser.password,
+                "mobile": addUser.mobile,
+                "name": addUser.name,
+                "role": addUser.role,
+                "email": addUser.email,
+                "superVisor": addUser.superVisor.id
+              }));
       if (response.statusCode == 201) {
         // var temp = userModelListFromJson(response.body);
         // users = temp;
@@ -250,7 +250,7 @@ class UserService extends ChangeNotifier {
       {required Map<String, dynamic> map, required String id}) async {
     try {
       var response = await tunnelRequest(
-          () => client.put(Uri.parse("$CustomIP.apiBaseUrl$userEndPoint/$id"),
+          () => client.put(Uri.parse("${CustomIP.apiBaseUrl}$userEndPoint/$id"),
               headers: {
                 "Content-Type": "application/json",
                 "x-access-token": loggedInUser.value!.accessToken!
