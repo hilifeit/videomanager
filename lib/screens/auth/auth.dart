33b596f3dfc3129d508d498be629cf80c23f25c9
rajiv24joshi <rajiv24joshi@gmail.com>
2022-08-03 17:11:52 +0545

    merged

--- lib/screens/auth/auth.dart
+++ lib/screens/auth/auth.dart
@@ -29,9 +29,9 @@ class AuthScreen extends ConsumerWidget {
                           ),
                         )),
                   )
-                : formState
-                    ? Login()
-                    : const ForgotPassword(),
+                : Center(
+                    child: formState ? Login() : const ForgotPassword(),
+                  ),
           ],
         ),
       ),
