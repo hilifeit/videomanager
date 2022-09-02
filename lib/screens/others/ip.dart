89a97d83201a62419f4e9843e2d1a683bd94c378
unknown <it@hilifefoodsmepal.com>
2022-09-02 10:32:02 +0545

    ssl

--- lib/screens/others/ip.dart
+++ lib/screens/others/ip.dart
@@ -1,30 +1,28 @@
 import 'package:universal_platform/universal_platform.dart';
 
-class CustomIP{
+class CustomIP {
   CustomIP._();
 
-   static get apiPort => 5000;
-   static get socketPort => 3000;
- 
+  static get apiPort => 5000;
+  static get socketPort => 3000;
+
   static get publicIp => "103.163.183.20";
-  static get localIp=> "192.168.1.74";
+  static get localIp => "192.168.1.74";
 
   static String get baseUrl {
-    
-    if(UniversalPlatform.isWeb)
-  {
-    // var url=Uri.base.toString().split(":")[1].replaceAll("//","http://");
-    final url=Uri.base.toString();
-    final finalUrl="http://${url.split('/')[2].split(':')[0]}";
-   
-    return finalUrl;
-  }
-  else{
+    if (UniversalPlatform.isWeb) {
+      // var url=Uri.base.toString().split(":")[1].replaceAll("//","http://");
+      final url = Uri.base.toString();
+      final finalUrl = "https://${url.split('/')[2].split(':')[0]}";
 
-    return const String.fromEnvironment("HOST");
+      return finalUrl;
+    } else {
+      return const String.fromEnvironment("HOST");
+    }
   }
-  } 
 
-  static String get apiBaseUrl=> "$baseUrl:$apiPort/v1/";
-  static String get socketBaseUrl=> "$baseUrl:$socketPort/";
+  static String get apiBaseUrl => "$baseUrl/api/";
+  // static String get apiBaseUrl=> "$baseUrl:$apiPort/v1/";
+  static String get socketBaseUrl => "$baseUrl/socket/";
+  // static String get socketBaseUrl=> "$baseUrl:$socketPort/";
 }
